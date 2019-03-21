dc9fa697cc6ca1085a4385e9649107b5e4311e04
KorawitRupanya <smart1micky268@gmail.com>
2019-03-21 14:04:19 +0700

    format

--- Tutorial4/flappy.py
+++ Tutorial4/flappy.py
@@ -1,81 +1,85 @@
 import arcade
-from models import World,Player
- 
+from models import World, Player
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
- 
+
+
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
- 
+
         super().__init__(*args, **kwargs)
- 
+
     def sync_with_model(self):
         if self.model:
             self.set_position(self.model.x, self.model.y)
- 
+
     def draw(self):
         self.sync_with_model()
         super().draw()
 
+
 class PillarPairSprite():
     def __init__(self, model):
         self.model = model
- 
+
         self.top_pillar_sprite = arcade.Sprite('images/pillar-top.png')
         self.bottom_pillar_sprite = arcade.Sprite('images/pillar-bottom.png')
 
     def draw(self):
         self.top_pillar_sprite.set_position(self.model.x, self.model.y + 400)
         self.top_pillar_sprite.draw()
- 
-        self.bottom_pillar_sprite.set_position(self.model.x, self.model.y - 400)
+
+        self.bottom_pillar_sprite.set_position(
+            self.model.x, self.model.y - 400)
         self.bottom_pillar_sprite.draw()
- 
+
+
 class FlappyDotWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
+
         arcade.set_background_color(arcade.color.WHITE)
- 
+
         self.start()
 
     def start(self):
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
- 
+
         self.dot_sprite = ModelSprite('images/1.png', model=self.world.player)
 
-        self.pillar_pair_sprites = [PillarPairSprite(model=self.world.pillar_pairs[0]),PillarPairSprite(model=self.world.pillar_pairs[1])]
+        self.pillar_pair_sprites = [PillarPairSprite(
+            model=self.world.pillar_pairs[0]), PillarPairSprite(model=self.world.pillar_pairs[1])]
 
-    
     def on_key_press(self, key, key_modifiers):
         if not self.world.is_started():
             self.world.start()
- 
+
         self.world.on_key_press(key, key_modifiers)
- 
+
     def update(self, delta):
         self.world.update(delta)
-        if self.world.is_dead() :
+        if self.world.is_dead():
             self.start()
- 
+
     def on_draw(self):
         arcade.start_render()
- 
+
         self.dot_sprite.draw()
-        for pillar_pair_sprite in self.pillar_pair_sprites :
+        for pillar_pair_sprite in self.pillar_pair_sprites:
             pillar_pair_sprite.draw()
 
         arcade.draw_text(str(self.world.score),
                          self.width - 30, self.height - 30,
                          arcade.color.BLACK, 20)
 
- 
- 
+
 def main():
     window = FlappyDotWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
- 
+
+
 if __name__ == '__main__':
     main()
--- Tutorial4/models.py
+++ Tutorial4/models.py
@@ -1,6 +1,7 @@
 from coldetect import check_player_pillar_collision
 from random import randint
 
+
 class Player:
     GRAVITY = 1
     STARTING_VELOCITY = 15
@@ -11,87 +12,87 @@ class Player:
         self.x = x
         self.y = y
         self.vy = Player.STARTING_VELOCITY
-    
+
     def jump(self):
         self.vy = Player.JUMPING_VELOCITY
- 
+
     def update(self, delta):
         self.y += self.vy
         self.vy -= Player.GRAVITY
-    
+
     def togkob(self):
-        return self.y==0 or self.y ==600
+        return self.y == 0 or self.y == 600
+
 
 class PillarPair:
     PILLAR_SPEED = 5
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
- 
+
     def update(self, delta):
         self.x -= PillarPair.PILLAR_SPEED
-        if self.x < -40 :
-                self.x = self.world.width+40
-                self.random_position_y()
+        if self.x < -40:
+            self.x = self.world.width+40
+            self.random_position_y()
         pass
-        
-    
 
     def hit(self, player):
         return check_player_pillar_collision(player.x, player.y,
                                              self.x, self.y)
- 
+
     def random_position_y(self):
-        self.y = randint(100,450)
+        self.y = randint(100, 450)
+
 
 class World:
     STATE_FROZEN = 1
     STATE_STARTED = 2
     STATE_DEAD = 3
 
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.pillar_pairs = [PillarPair(self, width - 100, height // 2),PillarPair(self, width + 350, height // 2)]
-        
+        self.pillar_pairs = [PillarPair(
+            self, width - 100, height // 2), PillarPair(self, width + 350, height // 2)]
+
         self.player = Player(self, width // 2, height // 2)
         self.state = World.STATE_FROZEN
         self.score = 0
- 
+
     def update(self, delta):
         if self.state in [World.STATE_FROZEN, World.STATE_DEAD]:
             return
- 
+
         self.player.update(delta)
         if(self.player.togkob()):
             self.die()
 
-        for pillar_pair in self.pillar_pairs :
+        for pillar_pair in self.pillar_pairs:
             pillar_pair.update(delta)
 
-            if pillar_pair.x == self.player.x :
-                self.score +=1 
+            if pillar_pair.x == self.player.x:
+                self.score += 1
 
             if pillar_pair.hit(self.player):
                 self.die()
-    
+
     def on_key_press(self, key, key_modifiers):
         self.player.jump()
-    
+
     def start(self):
         self.state = World.STATE_STARTED
- 
+
     def freeze(self):
-        self.state = World.STATE_FROZEN     
- 
+        self.state = World.STATE_FROZEN
+
     def is_started(self):
         return self.state == World.STATE_STARTED
 
     def die(self):
         self.state = World.STATE_DEAD
- 
+
     def is_dead(self):
         return self.state == World.STATE_DEAD
-
